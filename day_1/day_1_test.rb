ad9b99ce201d0088a35bb2074e367859d475c252
Bartek Głowacki <marathone@gmail.com>
2022-12-01 20:07:44 +0100

    Change test names

--- day_1/day_1_test.rb
+++ day_1/day_1_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require_relative "day_1"
 
-class Day1Test < Minitest::Test
+class DesignateElfWithMostCaloriesTest < Minitest::Test
   def test_1
     test_input = <<~TXT
       1000
@@ -23,6 +23,30 @@ class Day1Test < Minitest::Test
     elf = DesignateElfWithMostCalories.new.call(test_input)
 
     assert_equal( 24000, elf.carried_calories)
+  end
+end
+class DesignateTopThreeElvesTest < Minitest::Test
+  def test_1
+    test_input = <<~TXT
+      1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+    TXT
+
+    elves = DesignateTopThreeElves.new.call(test_input)
+
+    assert_equal( 41000, elves.sum(&:carried_calories))
   end
 end
 
